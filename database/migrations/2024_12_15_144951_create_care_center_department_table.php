450b0ffaeb441115174a9667f25caba66ffb16c9
abdelrahmanabdelsamie7 <abdelrhmanabdelsamie@gmail.com>
2025-01-20 04:30:37 +0200

    up2

--- database/migrations/2024_12_15_144951_create_care_center_department_table.php
+++ database/migrations/2024_12_15_144951_create_care_center_department_table.php
@@ -6,12 +6,12 @@ return new class extends Migration {
     public function up(): void
     {
         Schema::create('care_center_department', function (Blueprint $table) {
-            $table->uuid('id')->primary()->default(DB::raw('UUID()'));
-            $table->foreignUuid('department_id')->constrained('departments')->cascadeOnDelete()->cascadeOnUpdate();
-            $table->foreignUuid('care_center_id')->constrained('care_centers')->cascadeOnDelete()->cascadeOnUpdate();
+            $table->char('id', 36)->primary(); // إزالة default UUID()
+            $table->char('department_id', 36)->notNullable();
+            $table->char('care_center_id', 36)->notNullable();
             $table->timestamp('start_at')->nullable();
             $table->timestamp('end_at')->nullable();
-            $table->decimal('app_price', 10, 2)->default(0);
+            $table->decimal('app_price', 10, 2)->default(0)->notNullable();
             $table->unique(['department_id', 'care_center_id']);
             $table->timestamps();
         });
